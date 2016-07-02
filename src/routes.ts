34ca8cd066362948e567eaaad34de184153b3185
zape <peter.zahoran.76@gmail.com>
2016-07-02 18:34:33 +0200

    routing now uses hash instead of path.

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 import { provideRouter, RouterConfig }  from '@angular/router';
 import { SearchRepo } from './search';
 import { RepoDetail } from './repo';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 export default [
     provideRouter([
         { path: '', redirectTo: '/search', terminal: true }, 
         { path: 'search', component: SearchRepo },
         { path: 'repo/:owner/:repo', component: RepoDetail },
     ]),
+    {provide: LocationStrategy, useClass: HashLocationStrategy},
 ];
